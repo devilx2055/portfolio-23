d22c4032c92a08efa4f715d6b4ef8e169a09a6da
Priyanshu2055 <priyanshuvanra2@gmail.com>
2023-04-06 13:15:52 +0530

    add work page content and fix port issue

--- app.mjs
+++ app.mjs
@@ -1,10 +1,19 @@
 import express from "express";
 import path from "path";
+import logger from "morgan";
+import errorhandler from "errorhandler";
+import methodOverride from "method-override";
+import http from "http";
 
 const __dirname = path.resolve();
 const app = express();
 const port = 3000;
 
+const httpServer = http.createServer(app);
+
+app.use(logger("dev"));
+app.use(methodOverride());
+app.use(errorhandler());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
@@ -34,6 +43,12 @@ app.get("/contact", (req, res) => {
   });
 });
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+process.on("SIGTERM", () => {
+  httpServer.close(() => {
+    console.log("Closing http server.");
+  });
 });
